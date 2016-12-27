098d5bafec59039e86d88879eebb644d7ea00bd8
DengrongGuan12 <1031212913@qq.com>
2016-12-27 10:52:29 +0800

    what parser

--- ss_parser/src/main/java/parser/SSWhatParser.java
+++ ss_parser/src/main/java/parser/SSWhatParser.java
@@ -1,14 +1,37 @@
 package parser;
 
 import model.SSModel;
+import org.ansj.domain.Result;
+import org.ansj.domain.Term;
+import org.ansj.recognition.impl.NatureRecognition;
+import org.ansj.splitWord.analysis.BaseAnalysis;
+import org.ansj.splitWord.analysis.NlpAnalysis;
 import org.ansj.splitWord.analysis.ToAnalysis;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by DengrongGuan on 2016/12/26.
  */
 public class SSWhatParser extends SSParser{
     protected String extractKey(String ss){
-        return "what";
+        //词性标注规范 https://github.com/NLPchina/ansj_seg/wiki/%E8%AF%8D%E6%80%A7%E6%A0%87%E6%B3%A8%E8%A7%84%E8%8C%83
+        Result parserResult = NlpAnalysis.parse(ss);
+        Iterator<Term> it = parserResult.iterator();
+        StringBuilder builder = new StringBuilder();
+        while(it.hasNext()){
+            Term t = it.next();
+            String nature = t.getNatureStr();
+            if(nature.startsWith("n")){
+                if(nature.startsWith("nr") || nature.startsWith("ns")){
+                    continue;
+                }else{
+                    builder.append(t.getRealName()+";");
+                }
+            }
+        }
+        return builder.toString();
     }
     public void setKeyInSS(SSModel ssModel){
         String ss = ssModel.getValue();
@@ -16,8 +39,14 @@ public class SSWhatParser extends SSParser{
     }
 
     public static void main(String[] args){
-        String words = "中国是世界四大文明古国之一，有着悠久的历史，距今约5000年前，以中原地区为中心开始出现聚落组织进而成国家和朝代，后历经多次演变和朝代更迭，持续时间较长的朝代有夏、商、周、汉、晋、唐、宋、元、明、清等。中原王朝历史上不断与北方游牧民族交往、征战，众多民族融合成为中华民族。20世纪初辛亥革命后，中国的君主政体退出历史舞台，取而代之的是共和政体。1949年中华人民共和国成立后，在中国大陆建立了人民代表大会制度的政体。中国有着多彩的民俗文化，传统艺术形式有诗词、戏曲、书法和国画等，春节、元宵、清明、端午、中秋、重阳等是中国重要的传统节日。";
-        System.out.println(ToAnalysis.parse(words));
+        SSWhatParser ssWhatParser = new SSWhatParser();
+        String words = "1994年8月9日，原告刘某某向永吉县土地管理局递交《住宅用地申请书》，申请对位于缸窑镇二道村三社的房屋进行翻建。";
+        System.out.println(NlpAnalysis.parse(words));
+        System.out.println(ssWhatParser.extractKey(words));
+//        Result result = ToAnalysis.parse(words);
+//        List<Term> terms = result.getTerms();
+//        System.out.println(result.get(0).getNatureStr());
+
     }
 
 }
